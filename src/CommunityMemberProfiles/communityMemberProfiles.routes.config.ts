588d9152f1eb5990cfd07293e57072250d4dd963
2@Decentfaraz <gulfarazhassan535@gmail.com>
2022-09-28 05:23:17 +0500

    asdas

--- src/CommunityMemberProfiles/communityMemberProfiles.routes.config.ts
+++ src/CommunityMemberProfiles/communityMemberProfiles.routes.config.ts
@@ -1,7 +1,7 @@
 import express from 'express';
 import { CommonRoutesConfig } from '../common/common.routes.config';
 import CommunityMemberProfilesController from './controllers/communityMemberProfiles.controller';
-import jwtMiddleware from '../Auth/middleware/jwt.middleware';
+import jwtMiddleware from '../auth/middleware/jwt.middleware';
 
 export class CommunityMemberProfilesRoutes extends CommonRoutesConfig {
   constructor(app: express.Application) {
